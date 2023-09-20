e533547eea1bb211bec9a3319b900d9e1f02cc7f
pretraining4robotics <145623612+pretraining4robotics@users.noreply.github.com>
2023-09-21 09:49:41 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NeuRL-RMW.github.io
+Repository to host the website for "Neural Representation Learning for Robot Manipulation Workshop"
---
+++ assets/js/config.js
@@ -0,0 +1,52 @@
+const project_name = `Workshop for Neural Representation Learning for Robot Manipulation`
+const proj_small_caps = `<span class="small-caps">NeuRL-RMW</span>: `
+const conference_details = ['CoRL 2023', 'https://www.corl2023.org/', 'Muse 5', 'assets/images/corl23_logo.png']
+const workshop_date = `November 6, 2023`
+
+const talk_speaker_details = [
+  ['Shuran Song', './assets/images/talks/shuran_song.jpg', 'Assistant Professor', 'Stanford University', 'https://shurans.github.io/'],
+  ['David Held', './assets/images/talks/david_held.jpg', 'Associate Professor', 'Carnegie Mellon University', 'http://davheld.github.io'],
+  ['Georgia Chalvatzaki', './assets/images/talks/Georgia-Chalvatzaki.jpg', 'Assistant Professor', 'TU Darmstadt', 'https://gchal.github.io'],
+  ['Pete Florence', './assets/images/talks/pete-florence.png', 'Senior Research Scientist', 'Google Deepmind', 'https://www.peteflorence.com'],
+  ['Chris Paxton', './assets/images/talks/chris-paxton.jpeg', 'Research Scientist', 'Meta AI', 'https://cpaxton.github.io'],
+  ['Chad Jenkins', './assets/images/talks/chad_jenkins.jpg', 'Professor', 'University of Michigan', 'https://robotics.umich.edu/profile/chad-jenkins'],
+]
+
+const organizers_details = [
+  ['Karthik Desingh', 'assets/images/organizers/karthik_desingh.jpeg', 'Assistant Professor', 'University of Minnesota, Twin Cities', 'https://karthikdesingh.com'],
+  ['Changhyun Choi', 'assets/images/organizers/Changhyun_Choi.jpg', 'Assistant Professor', 'University of Minnesota, Twin Cities', 'https://cse.umn.edu/ece/changhyun-choi'],
+  ['Yu Xiang', 'assets/images/organizers/Yu_Xiang.jpg', 'Assistant Professor', 'The University of Texas at Dallas', 'https://yuxng.github.io'],
+  ['Jishnu Jaykumar Padalunkal', 'assets/images/organizers/Jishnu-Jaykumar-Padalunkal.jpg', 'PhD Student', 'The University of Texas at Dallas', 'https://jishnujayakumar.github.io'],
+  ['Ninad Khargonkar', 'assets/images/organizers/Ninad-Khargonkar.jpg', 'PhD Student', 'The University of Texas at Dallas', 'https://kninad.github.io'],
+  ['Sai Haneesh Allu', 'assets/images/organizers/Sai-Haneesh-Allu.png', 'PhD Student', 'The University of Texas at Dallas', 'https://www.linkedin.com/in/sai-haneesh-allu'],
+  ['Alireza Rezazadeh', 'assets/images/organizers/Alireza-Rezazadeh.png', 'PhD Student', 'University of Minnesota, Twin Cities', 'https://www.alireza.page'],
+]
+
+const accepted_presentations = [
+  ["FullName-1", "#", "https://bulma.io/images/placeholders/128x128.png", "Designation", "Affiliation", "Presentation Title", "Spotlight"],
+  ["FullName-2", "#", "https://bulma.io/images/placeholders/128x128.png", "Designation", "Affiliation", "Presentation Title", ""],
+]
+
+const org_affiliation_logos = [
+  'assets/images/affiliation-logos/um-logo.png',
+  'assets/images/affiliation-logos/utd-logo.png',
+]
+
+
+const schedule = [
+  ['intro', '08:30 - 08:35', 'Introduction'],
+  ['inv-talk', '08:35 - 09:00', 'Invited Talk'],
+  ['inv-talk', '09:00 - 09:30', 'Invited Talk'],
+  ['inv-talk', '09:30 - 10:00', 'Invited Talk'], 
+  ['spot-ppt', '10:00 - 10:30', 'Spotlight Presentations'], 
+  ['coffee-break', '10:30 - 11:00', 'Coffee Break'],
+  ['inv-talk', '11:00 - 11:30', 'Invited Talk'],
+  ['spot-poster', '11:30 - 12:30', 'Spotlight Posters | Muse 1'],
+  ['lunch-break', '12:30 - 13:30', 'Lunch Break'],
+  ['inv-talk', '13:30 - 14:00', 'Invited Talk'],
+  ['spot-ppt', '14:00 - 15:00', 'Spotlight presentations'],
+  ['spot-poster', '15:00 - 15:30', 'Spotlight Posters | Muse 1'],
+  ['coffee-break', '15:30 - 16:00', 'Coffee Break'],
+  ['inv-talk', '16:00 - 16:30', 'Invited Talk'],
+  ['disc', '16:30 - 17:30', 'Panel Discussion'], 
+]
